823f66b1c7041ab2a7d1edead0f041b59a753ded
Melanie Bruce <mbruce.peace@gmail.com>
2023-03-09 11:40:12 -0500

    remove pwa capability

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import { Quasar } from "quasar";
-import quasarUserOptions from "./quasar-user-options";
-import './registerServiceWorker'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import { Quasar } from 'quasar';
+import quasarUserOptions from './quasar-user-options';
 
-const app = createApp(App)
-app.use(store)
-app.use(router)
-app.use(Quasar, quasarUserOptions)
-app.mount('#app')
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.use(Quasar, quasarUserOptions);
+app.mount('#app');
